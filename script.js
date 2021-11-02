dce2024fd055719dfa9a76278b65d221bc1c1e45
Youri Bakker <youriboulanger@gmail.com>
2021-11-02 14:41:57 +0100

    made the obj fully work

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-const yahtzee = () => {
+const Yahtzee = () => {
+
+    //local scoped variables
     let obj = {};
     obj.arr = [];
-   obj.getRandomNumbers = (imgAmount) => {
-         obj.arr = [];
+
+    //generates random numbers
+    obj.getRandomNumbers = (imgAmount) => {
+        obj.arr = [];
         for (let i = 0; i < imgAmount; i++) {
             let result = Math.floor(Math.random() * 10);
 
             while (result > 6 || result === 0) {
-                result =  Math.floor(Math.random() * 10);
+                result = Math.floor(Math.random() * 10);
             }
             obj.arr[i] = result;
         }
         return obj.arr;
     }
 
+    //makes the new img takes based on the amount given
     obj.makeImgTags = (amount) => {
 
-        if (document.querySelectorAll("img").length >= amount){
+        if (document.querySelectorAll("img").length >= amount) {
 
         } else {
             let id = document.getElementById("fotoCollection")
-            for (let i = 0; i < amount; i++){
-                let img =  document.createElement("img");
+            for (let i = 0; i < amount; i++) {
+                let img = document.createElement("img");
                 id.appendChild(img);
             }
         }
         obj.fillImgTags();
     }
 
+    //fills the img tags with the needed img
     obj.fillImgTags = () => {
         let imgTags = document.querySelectorAll("img");
         let arr = obj.getRandomNumbers(imgTags.length);
-        for (let i = 0; i < imgTags.length; i++){
+        for (let i = 0; i < imgTags.length; i++) {
             imgTags[i].setAttribute("src", `img/dice_${arr[i]}.jpg`);
         }
         console.log(arr + "\r\n --------------")
     }
+
+    return obj;
 }
 
-let objYathzee =  yahtzee();
+let objYathzee = Yahtzee();
