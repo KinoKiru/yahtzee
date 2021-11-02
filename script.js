aa61ccc59d71c022ad78a5ae4b3e4c9817cf6734
Youri Bakker <youriboulanger@gmail.com>
2021-11-02 14:00:00 +0100

    made the random numbers

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let click = () => {
+function getRandomNumbers() {
+    let arr = [];
+    for (let i = 0; i < 5; i++) {
+        let result = Math.floor(Math.random() * 10);
 
+        while (result > 6 || result === 0) {
+            result =  Math.floor(Math.random() * 10);
+        }
+        arr[i] = result;
+        console.log(arr[i]);
+    }
 }
