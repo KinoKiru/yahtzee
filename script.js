e13c7a359db57bf1ad982d40a261ed3dd91179e6
Youri Bakker <youriboulanger@gmail.com>
2021-11-02 14:15:30 +0100

    made the fuction for making tags and filling them

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function getRandomNumbers() {
+function getRandomNumbers(imgAmount) {
     let arr = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < imgAmount; i++) {
         let result = Math.floor(Math.random() * 10);
 
         while (result > 6 || result === 0) {
@@ -8,5 +8,21 @@ function getRandomNumbers() {
         }
         arr[i] = result;
         console.log(arr[i]);
+    }
+    return arr;
+}
+
+function makeImgTags (amount) {
+    for (let i = 0; i < amount; i++){
+         document.createElement("img");
+    }
+    fillImgTags();
+}
+
+function fillImgTags () {
+    let imgTags = document.querySelectorAll("img");
+    let arr = getRandomNumbers(imgTags.length);
+    for (let i = 0; i < imgTags.length; i++){
+        imgTags[i].setAttribute("src", `img/${arr[i]}.png`);
     }
 }
